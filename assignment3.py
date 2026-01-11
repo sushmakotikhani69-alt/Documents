809b2716f9c55a82a89b6801c8d22debaa26c9e9
sushmakotikhani69-alt <sushmakotikhani69@gmail.com>
2026-01-11 13:28:05 +0530

    assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,6 @@
+username=input("enter a username:")
+password=input("enter a password:")
+if username=="bhakti" and password=="bhakti@05":
+      print("login sucessfull")
+else:
+      print("access deined")
